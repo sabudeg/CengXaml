bb107e95b44a95e7c2022f8938608863afb0f6c4
sabudeg <bburakdegirmenci@gmail.com>
2018-11-05 02:04:38 +0300

    first commit

---
+++ CengDepartment/CengDepartment/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿
+using Xamarin.Forms;
+
+namespace CengDepartment
+{
+    public partial class MainPage : TabbedPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
